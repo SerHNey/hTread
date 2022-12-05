5e59a74197a28bada86edce0a1c112b44e42e77e
Нечто Страшное <naumov-1212@Mail.ru>
2022-12-05 12:25:21 +0300

    dddd

--- hTread/hTread.c
+++ hTread/hTread.c
@@ -88,15 +88,27 @@ char* add(tm* u1)
     return (s2);
 }
 
-char* settime(tm* u)
+void* settime(tm* u)
 {
     char s[20];
-    char* tmp;
-    for (int i = 0; i < 20; i++) s[i] = 0;
-    int length = strftime(s, 20, "%H:%M:%S", u);
-    tmp = (char*)malloc(sizeof(s));
-    strcpy(tmp, s);
-    return(tmp);
+    while (1)
+    {
+        u->tm_sec += 1;
+        if (u->tm_sec == 60) {
+            u->tm_sec = 0;
+            u->tm_min += 1;
+            if (u->tm_min == 60) {
+                u->tm_min = 0;
+                u->tm_hour += 1;
+            }
+        }
+        for (int i = 0; i < 20; i++) s[i] = 0;
+        int length = strftime(s, 20, "%H:%M:%S", u);
+        Sleep(1000);
+        system("cls");
+        printf("Время запущено: ");
+        printf("%s\n", s);
+    }
 }
 
 int main()
@@ -107,13 +119,7 @@ int main()
     //secondsmer();
 
     
-    //tm* u;
-    //char* f;
-    //system("cls");
-    //const time_t timer = NULL;
-    //u = localtime(&timer);
-    //f = settime(u);
-    //puts(f);
+
 
     //return 0;
 
@@ -128,18 +134,48 @@ int main()
     tm timeE;
     timeE.tm_sec = time(NULL);
     printf("%ld\n", timeE.tm_sec);
-    printf("Выберите действие над часами\n1 - пауза \n2 - настройка времени \n3 - секундомер\n4 - таймер\n5 - Запуск времени\n");
+    printf("Выберите действие над часами\n1 - Запуск времени \n2 - настройка времени \n3 - секундомер\n4 - таймер\n5 - Пауза\n");
     printf("0 - Остановить\n");
+    tm* u;
+    char* f;
     while (1)
     {
         scanf_s("%d", &choose);
         switch (choose)
         {
         case 1:
-            
+            system("cls");
+            const time_t timer = time("10:10:10");
+            u = localtime(&timer);
+            hTread[0] = CreateThread(NULL, 0, settime,u, NULL, NULL);
         break;
         case 2:
+            printf("Введите время в формате - ЧЧ;ММ;CC\n");
+            char* stroka = calloc(30, sizeof(char));
+            scanf_s("%s", stroka);
 
+            char* number = " ";
+
+            int numbers[3];
+            numbers[0] = atof(number);
+            int  i = 1;
+            while (number != NULL)
+            {
+                number = strtok(NULL, ":");
+                if (number != NULL) {
+                    if (number[0] != '\r') {
+                        numbers[i] = atoi(number);
+                        i++;
+                    }
+                }
+                if (i > 3)
+                    break;
+            }
+            tm* u = NULL;
+            u->tm_hour = 12;
+            u->tm_min = numbers[1];
+            u->tm_sec = numbers[2];
+            settime(u);
         break;
         case 3:
             hTread[0] = CreateThread(NULL, 0, secondsmer, 0, NULL, NULL);
